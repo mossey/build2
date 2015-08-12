28717a947aba60e31b25937164c0b384a1e24254
mossey <qwerty41>
2015-08-12 19:57:20 +0300

    insert into Database minus the real stripe token

--- processing.php
+++ processing.php
@@ -12,7 +12,21 @@
 
 // these are the details from the the person booking the ticket
 
-
+$radio=$_POST['sessions'];
+$ePrice=$_POST['price'];
+$pSize=$_POST['party'];
+$amountPaid=$pSize*$ePrice;
+$methodd=$_POST['payments'];
+echo $radio;
+echo $methodd;
+if($methodd=="mpesa"){
+    $status="not confirmed";
+    $code=$_POST['code'];
+}
+else{
+    //stripe value$code=$_POST[''];
+    $status=="confirmed";
+}
 
 
 $servername = "localhost";
@@ -22,12 +36,12 @@ $dbname = "book";
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
+    // set the PDO error mode to exc    eption
 
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = "INSERT INTO attendees (fName, lName, email)
-    VALUES ('".$_POST['fName']."','".$_POST['lName']."','".$_POST['email']."')";
+    $sql = "INSERT INTO bookings (payMethod, code, eventID, Paid, SittingID,amountPaid)
+    VALUES ('".$_POST['payments']."','".$_POST['code']."','".$_POST['eventID']."','$status','".$_POST['sessions']."',$amountPaid)";
     // use exec() because no results are returned
     $conn->exec($sql);
     echo "New record created successfully";
