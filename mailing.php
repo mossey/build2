85e7ea078ee1a42a8c32300e337702a04f940c95
mossey <qwerty41>
2015-08-13 08:49:13 +0300

    here i added mailing

--- mailing.php
+++ mailing.php
@@ -1,4 +1,34 @@
-<!DOCTYPE html>
+<?php
+
+require_once "vendor/autoload.php";
+
+$mail = new PHPMailer;
+
+//Enable SMTP debugging.
+$mail->SMTPDebug = 3;
+//Set PHPMailer to use SMTP.
+$mail->isSMTP();
+//Set SMTP host name
+$mail->Host = "smtp.gmail.com";
+//Set this to true if SMTP host requires authentication to send email
+$mail->SMTPAuth = true;
+//Provide username and password
+$mail->Username = "nandwa.moses@gmail.com";
+$mail->Password = "M41NANDWA";
+//If SMTP requires TLS encryption then set it
+$mail->SMTPSecure = "tls";
+//Set TCP port to connect to
+$mail->Port = 587;
+
+$mail->From = "nandwa.moses@gmail.com";
+$mail->FromName = "Full Name";
+
+$mail->addAddress($_POST['email']);
+
+$mail->isHTML(true);
+
+$mail->Subject = "Subject Text";
+$mail->Body = "<!DOCTYPE html>
 <html lang="en">
 	<head>
 		<title>Gizani</title>
@@ -21,12 +51,12 @@
                 	<h2>Gizani</h2>
                     <p> Hi, </p>
                     <p>
-                            You have succesifully booked a ticket for Gizani Dinning in the Dark
-                    </p>
+You have succesifully booked a ticket for Gizani Dinning in the Dark
+</p>
                     <p>
-                        regards,
+regards,
                         <br>
-                        Gizani Team.
+Gizani Team.
                     </p>
                 </div>
                 <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
@@ -43,4 +73,14 @@
 		<!-- Bootstrap JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 	</body>
-</html>
+</html>";
+$mail->AltBody = "This is the plain text version of the email content";
+
+if(!$mail->send())
+{
+    echo "Mailer Error: " . $mail->ErrorInfo;
+}
+else
+{
+    echo "Message has been sent successfully";
+}
--- processing.php
+++ processing.php
@@ -29,6 +29,77 @@ else{
 }
 
 
+require_once "vendor/autoload.php";
+$mail = new PHPMailer();
+
+$mail->IsSMTP();                                      // set mailer to use SMTP
+$mail->Host = "smtp.gmail.com";  // specify main and backup server
+$mail->SMTPAuth = true;     // turn on SMTP authentication
+$mail->Username = "nandwa.moses@gmail.com";  // SMTP username
+$mail->Password = "M41NANDWA"; // SMTP password
+
+$mail->From = "moses@gizani.com";
+$mail->FromName = "Gizanir";
+$mail->AddAddress ($_POST['email']);
+                  // name is optional
+$mail->AddReplyTo("moses@gizani.com");
+
+$mail->WordWrap = 50;                                 // set word wrap to 50 characters
+    // optional name
+$mail->IsHTML(true);                                  // set email format to HTML
+
+$mail->Subject = "Thanks";
+$mail->Body    = file_get_contents("emal.html");
+//$mail->AltBody = "This is the body in plain text for non-HTML mail clients";
+
+if(!$mail->send())
+{
+    echo "Mailer Error: " . $mail->ErrorInfo;
+}
+else
+{
+    echo "Message has been sent successfully";
+}
+/*
+\Stripe\Stripe::setApiKey("pk_test_epuy9kWiGoIklHviDACQpUiw");
+
+// Get the credit card details submitted by the form
+$token = $_POST['stripeToken'];
+
+// Create the charge on Stripe's servers - this will charge the user's card
+try {
+    $charge = \Stripe\Charge::create(array(
+            "amount" => 1000, // amount in cents, again
+            "currency" => "usd",
+            "source" => $token,
+            "description" => "Example charge")
+
+
+$mail->addAddress($_POST['email']);
+//Set PHPMailer to use SMTP.
+$mail->isSMTP();
+//Set SMTP host name
+$mail->Host = "smtp.gmail.com";
+//Set this to true if SMTP host requires authentication to send email
+$mail->SMTPAuth = true;
+//Provide username and password
+$mail->Username = "nandwa.moses@gmail.com";
+$mail->Password = "M41NANDWA";
+//If SMTP requires TLS encryption then set it
+$mail->SMTPSecure = "tls";
+//Set TCP port to connect to
+$mail->Port = 587;
+
+$mail->From = "nandwa.moses@gmail.com";
+$mail->FromName = "Full Name";
+
+$mail->isHTML(true);
+$mail->Subject = "Subject Text";
+    );
+} catch(\Stripe\Error\Card $e) {
+    // The card has been declined
+}
+/*
 $servername = "localhost";
 $username = "root";
 $password = "qwerty41";
@@ -56,3 +127,4 @@ $conn = null;
 
 // do emails
 
+*/
